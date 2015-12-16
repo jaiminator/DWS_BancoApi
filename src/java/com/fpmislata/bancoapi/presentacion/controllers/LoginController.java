044a4b2580a2d313bc280d65b6acac445fc83496
alumno <alumno@172.16.205.114>
2015-12-16 09:11:36 +0100

    Implementaciones API

--- src/java/com/fpmislata/bancoapi/presentacion/controllers/LoginController.java
+++ src/java/com/fpmislata/bancoapi/presentacion/controllers/LoginController.java
@@ -3,11 +3,13 @@ package com.fpmislata.bancoapi.presentacion.controllers;
 
 import com.fpmislata.banco.business.domain.Rol;
 import com.fpmislata.banco.business.domain.Usuario;
+import com.fpmislata.banco.security.PasswordManager;
 import com.fpmislata.bancoapi.presentacion.security.WebSession;
 import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,12 +21,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
     
+    @Autowired
+    PasswordManager passwordManager;
+    
     @RequestMapping(value = "/login", method=RequestMethod.GET, produces = "application/json")
     public void login(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         try {
             HttpSession httpSession = httpServletRequest.getSession();
             
-            Usuario usuario = new Usuario(1, "jaime", "admin", Rol.administrador);
+            Usuario usuario = new Usuario("Juan", passwordManager.encrypt("password"));
+            usuario.setRol(Rol.Administrador);
+            
             WebSession webSession = new WebSession(usuario, new Date());
             
             httpSession.setAttribute("webSession", webSession);
